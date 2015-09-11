4ae76f22d249bd3b0044eeed6e5f505788b1441d
doris defar <ddefar@doriss-MacBook-Pro.local>
2015-09-11 16:32:22 +0200

    included rails-starterkit local cookbook

--- site-cookbooks/rails-starterkit/recipes/default.rb
+++ site-cookbooks/rails-starterkit/recipes/default.rb
@@ -6,25 +6,11 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-execute 'fetch signing key' do
-  command 'gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3'
-end
-execute 'install rvm' do
-	command 'curl -sSL https://get.rvm.io | bash -s stable'
-end
-#execute 'source rvm' do
-#  command 'source /home/vagrant/.rvm/scripts/rvm'
-#end
-
-
-execute 'install ruby' do
-  command 'rvm install 2.2.1'
-end
-execute 'set default ruby version' do
-  command 'rvm use 2.2.1 --default'
-end
-
-execute 'install rails gem' do
-  command 'sudo gem install rails'
-  not_if 'gem list | grep rails'
+bash "create sample app" do
+  code <<-EOH
+    cd /vagrant
+    rails new getting-started
+    cd getting-started
+    rails s -p 0.0.0.0
+  EOH
 end
